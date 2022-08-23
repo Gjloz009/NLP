dc981223d582e0a6a7f7678d9810f7a4b429273a
Gustavo Jafet <61368498+Gjloz009@users.noreply.github.com>
2022-08-22 19:02:18 -0500

    instalaciones requeriddad

--- tweets analysis.py
+++ tweets analysis.py
@@ -8,7 +8,19 @@ Librerías:
 -pandas
 -nlptk
 
+necesario tener registro en la api de twiter para poder ddescargar los proyectos
 '''
 
+import tweepy
+pip install tweepy
+# Agregamos las llaves necesarias 
+# nunca dejar estas llaves publicas
+consumer_key = "intorduce tus llaves"
+consumer_secret = 'introduce la otra lllavve'
+accces_key = 'tres llaves'
+acces_secret = 'ultima llave'
 
+# autenticación de la api
+auth = tweepy.OAuth
 
+pip install tweepy
